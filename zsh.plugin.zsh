a2becefd029b05c21e58a651e99853e4d5d03874
pd9333 <pd9333@gmail.com>
2021-08-20 20:10:44 +0800

    feat(fasd): cache zsh-hook of fasd

--- zsh.plugin.zsh
+++ zsh.plugin.zsh
@@ -1,3 +1,11 @@
 setopt AUTO_CD
 setopt AUTO_PUSHD
 setopt PUSHD_IGNORE_DUPS
+
+# fasd integration with only zsh-hook
+fasd_cache="${ZSH_CACHE_DIR}/fasd-init-cache"
+if [[ "$commands[fasd]" -nt "$fasd_cache" || ! -s "$fasd_cache" ]]; then
+    fasd --init zsh-hook >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
